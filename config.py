8313bd6e240ad7a82d888c76f2a2c4fa9be4b71a
velpularohith <98519138+velpularohith@users.noreply.github.com>
2024-12-17 19:58:58 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+snowflake_user = "PHOENIX"
+snowflake_password = "Phoenix@1"
+snowflake_account = "tb80036.central-india.azure"
+snowflake_warehouse = "COMPUTE_WH"
+snowflake_database = "PHOENIX_DB"
+snowflake_schema = "PHOENIX_SC"
